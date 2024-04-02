294a88aec2a0c68cf53d6373d3003230339000d1
UMRGRS <qxrvynilp@gmail.com>
2024-04-02 10:43:21 -0600

    Everything is done

--- app/src/main/java/mx/edu/utch/mdapp/MainActivity.kt
+++ app/src/main/java/mx/edu/utch/mdapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package mx.edu.utch.mdapp
 
+import android.content.DialogInterface
 import android.graphics.Color
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
@@ -14,9 +15,11 @@ import android.view.View
 import android.view.View.OnClickListener
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import mx.edu.utch.mdapp.databinding.ActivityMainBinding
 import java.util.Collections
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     private var clicked:Boolean = true
@@ -69,30 +72,32 @@ class MainActivity : AppCompatActivity() {
                 binding!!.gameZone.im43
                 )
         )
-        Collections.shuffle(deck)
+        binding!!.scoreZone.mainActivityTvPlayer1.setTypeface(null,Typeface.BOLD_ITALIC)
+        binding!!.scoreZone.mainActivityTvPlayer2.setTypeface(null,Typeface.BOLD_ITALIC)
         startOn()
         clickOn()
         binding!!.fabPrincipal.setOnClickListener{
-            Toast.makeText(this,"Fab prin", Toast.LENGTH_SHORT).show()
-            binding!!.gameZone.im11.setImageResource(R.drawable.day)
+            endOpt()
         }
-        
         setSupportActionBar(binding!!.mainBottomAppBar)
     }
 
     private fun startOn() {
         if(turno){
             binding!!.scoreZone.mainActivityTvPlayer1.setBackgroundColor(Color.GREEN)
+            binding!!.scoreZone.mainActivityTvScorePlayer1.setBackgroundColor(Color.GREEN)
             binding!!.scoreZone.mainActivityTvPlayer2.setBackgroundColor(Color.TRANSPARENT)
+            binding!!.scoreZone.mainActivityTvScorePlayer2.setBackgroundColor(Color.TRANSPARENT)
         }
         else{
             binding!!.scoreZone.mainActivityTvPlayer1.setBackgroundColor(Color.TRANSPARENT)
+            binding!!.scoreZone.mainActivityTvScorePlayer1.setBackgroundColor(Color.TRANSPARENT)
             binding!!.scoreZone.mainActivityTvPlayer2.setBackgroundColor(Color.GREEN)
+            binding!!.scoreZone.mainActivityTvScorePlayer2.setBackgroundColor(Color.GREEN)
         }
-        binding!!.scoreZone.mainActivityTvPlayer1.setTypeface(null,Typeface.BOLD_ITALIC)
-        binding!!.scoreZone.mainActivityTvPlayer2.setTypeface(null,Typeface.BOLD_ITALIC)
     }
     private fun clickOn() {
+        Collections.shuffle(deck)
         for (i in (0..<images!!.size)){
             images!![i]!!.setOnClickListener{
                 images!![i]!!.setImageResource(deck[i])
@@ -112,10 +117,31 @@ class MainActivity : AppCompatActivity() {
             var handler = Handler(Looper.getMainLooper())
             handler.postDelayed({
                 if(card == first_image){
+                    if(turno){
+                        score1 += 1
+                        binding!!.scoreZone.mainActivityTvScorePlayer1.text = "Puntos: $score1"
+                    }
+                    else{
+                        score2 += 1
+                        binding!!.scoreZone.mainActivityTvScorePlayer2.text = "Puntos: $score2"
+                    }
                     first_card!!.isVisible = false
                     img.isVisible = false
-                    startOn()
-                    xtivate(true)
+                    if(endGame()){
+                        if(score1>score2){
+                            showDialogAlertSimple("Ganador!","Ganó el jugador 1")
+                        }
+                        else if(score2>score1){
+                            showDialogAlertSimple("Ganador!","Ganó el jugador 2")
+                        }
+                        else if(score1==score2){
+                            showDialogAlertSimple("No one wins!","Empate :c")
+                        }
+                    }
+                    else{
+                        startOn()
+                        xtivate(true)
+                    }
                 }
                 else{
                     first_card!!.setImageResource(R.drawable.reverso)
@@ -135,27 +161,93 @@ class MainActivity : AppCompatActivity() {
             images!![i]!!.isEnabled = b
         }
     }
+    private fun endGame():Boolean{
+        for (i in (0..<images!!.size)){
+            if(images!![i].isVisible){
+                return false
+            }
+        }
+        return true
+    }
+    private fun makeVisible(){
+        for (i in (0..<images!!.size)){
+            images!![i]!!.isVisible = true
+            images!![i]!!.setImageResource(R.drawable.reverso)
+        }
+    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId){
             R.id.option_1 ->{
-                newGame()
+                restart()
                 true
             }
             R.id.option_2->{
-                showHelp()
+                endOpt()
+                true
+            }
+            R.id.option_3->{
+                exit()
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
     }
 
-    private fun showHelp() {
-        Log.e("Click","opt 1")
+    private fun restart() {
+        makeVisible()
+        clickOn()
+        xtivate(true)
+        clicked = true
+        turno = true
+        score1 = 0
+        score2 = 0
+        binding!!.scoreZone.mainActivityTvScorePlayer1.text = "Puntos: $score1"
+        binding!!.scoreZone.mainActivityTvScorePlayer2.text = "Puntos: $score2"
+        startOn()
     }
 
-    private fun newGame() {
-        Log.e("Click","opt 2")
+    private fun endOpt() {
+        AlertDialog.Builder(this)
+            .setTitle("Resultados")
+            .setMessage("Jugador 1: $score1 \nJugador 2: $score2")
+            .setPositiveButton(
+                getString(R.string.newgame),
+                DialogInterface.OnClickListener { _, _ ->
+                    restart()
+                })
+            .setNegativeButton(
+                getString(R.string.salir),
+                DialogInterface.OnClickListener { _, _ ->
+                    exit()
+                })
+            .setCancelable(false)
+            .show()
+    }
+    private fun exit(){
+        showDialogAlertSimple("Salir", "¿Estas seguro?")
+    }
+    private fun endApp(){
+        finishAffinity()
+        exitProcess(0)
+    }
+
+    private fun showDialogAlertSimple(title:String, msg:String) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(msg)
+            .setPositiveButton(
+                getString(R.string.continuar),
+                DialogInterface.OnClickListener { _, _ ->
+                    //botón continuar pulsado
+                })
+            .setNegativeButton(
+                getString(R.string.salir),
+                DialogInterface.OnClickListener { _, _ ->
+                    endApp()
+                })
+            .setCancelable(false)
+            .show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
